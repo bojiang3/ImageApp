df10519a8b59fdee52016d2178ea519a88758efd
bojiang3 <bojiang3@illinois.edu>
2023-03-07 11:01:47 -0500

    Upload the project

---
+++ Image3/Image3App.swift
@@ -0,0 +1,17 @@
+//
+//  Image3App.swift
+//  Image3
+//
+//  Created by Melo on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Image3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Image3UITests/Image3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Image3UITestsLaunchTests.swift
+//  Image3UITests
+//
+//  Created by Melo on 1/28/23.
+//
+
+import XCTest
+
+final class Image3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
